5c53c9a03742b506330cf8919fe407e5faaafca8
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 17:51:07 -0300

    Centraliza configuración de invitación Granja de Zenón

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Juancito",
+    ageLabel: "CUMPLE 2 AÑOS",
+    message: "Te invito a que pasemos una tarde muy divertida celebrando mi cumpleaños.",
+    eventDateTime: "2027-01-16T16:00:00-03:00",
+    eventEndDateTime: "2027-01-16T20:00:00-03:00",
+    dateLabel: "Sábado 16 de Enero",
+    timeLabel: "16:00 a 20:00 hs.",
+    venue: "Janos Hurlingham",
+    address: "Gral. Pedro Díaz 1800, Hurlingham",
+    mapUrl: "https://www.google.com/maps/search/?api=1&query=Janos+Hurlingham+Gral.+Pedro+D%C3%ADaz+1800+Hurlingham",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Juancito. 🐮🌾",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/sounds/audio3860.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/foto1.jpg",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/1.png",
+      width: "min(112vw, 520px)",
+      top: "62px",
+      translateX: "-51%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/4.png",
+      width: "min(74vw, 340px)",
+      top: "28px",
+      translateX: "-52%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/3.png",
+      width: "min(108vw, 500px)",
+      top: "2px",
+      translateX: "-51%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionGranjaDeZenon/main/www.tarjetas.cool/modelos/cumple/granja_de_zenon/img/2.png",
+      width: "min(66vw, 305px)",
+      top: "42px",
+      translateX: "-50%"
+    }
+  ]
+};
